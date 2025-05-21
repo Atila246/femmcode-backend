02fde5840194302e90aa4864a588c3513e199d3a
Atila246 <thalitinha246@gmail.com>
2025-05-21 15:44:41 -0300

    Back-end com link para o front

--- meu-app-login/index.js
+++ meu-app-login/index.js
@@ -1,3 +1,6 @@
+const cors = require('cors');
+app.use(cors({ origin: 'http://localhost:5173' }));
+
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
